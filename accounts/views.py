5adca49618f53c177ccbc3943749c1c7aa8ccbb3
[firstname <dryekta1979.developer@gmail.com>
2024-12-18 12:33:52 +0330

    Django-bootstrap template in forms- class 22-08-1403-۲

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import UserRegisterForm
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -20,8 +21,17 @@ def user_register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('home')
+            data=form.cleaned_data
+            User.objects.create_user(
+                user_name=data['username'],
+                email=data['email'],
+                password_2=data['password'],
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                username=data['username'],
+
+
+            )
     else:
         form = UserRegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
